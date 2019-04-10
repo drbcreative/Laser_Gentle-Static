245343e8a6b42f633efacca4e943a3e5aae247c4
Danny <holler@dannyburns.me>
2019-04-09 23:21:20 -0700

    hero section complete with slider

--- js/slider-init.js
+++ js/slider-init.js
@@ -1,17 +1,9 @@
 $(document).ready(function() {
-  $(".testimonial-container").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: true,
-    dots: true,
-    autoplay: true,
-    arrows: false
-  });
   $(".hero").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     infinite: !0,
-    dots: false,
+    dots: true,
     autoplay: true,
     arrows: false,
     autoplaySpeed: 5000
